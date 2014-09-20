ae098b2b339bfc9fcd331f8b5fb48d3078cce0f4
Harry Scheiner <harryscheiner@gmail.com>
2014-09-20 18:07:17 -0400

    Changed array syntax back for php < 5.4

--- partials/playerhead.php
+++
@@ -1,8 +0,0 @@
-<div class="player <?=$player->uuid?>">
-	<div class="player-head-container">
-		<img class="loader" src="img/loader.gif" alt=""/>
-	</div>
-	<div class="player-name">
-		<?=$player->name?><br/>
-	</div>
-</div>
