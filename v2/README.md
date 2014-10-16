0fa6b6cdb24eda8a84246726cfabfcb7aaad1c31
Harry <harryscheiner@gmail.com>
2014-10-16 17:04:40 -0400

    Initial work on V2

---
+++ v2/README.md
@@ -0,0 +1,2 @@
+mc.spectrumbranch.com
+=====================
---
+++ v2/index.php
@@ -0,0 +1,49 @@
+<?php
+	include('config.php');
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie10 lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Version 2</title>
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+
+		<meta property="og:title" content="Apocalypse Server" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://mc.spectrumbranch.com/" />
+		<meta property="og:site_name" content="catnips" />
+		<meta property="og:image" content="http://mc.spectrumbranch.com/img/egg.png" />
+		<meta property="og:description" content="See who's whitelisted, who's online, and more!" />
+
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+		<link rel="stylesheet" href="<?=($fn='css/bootstrap.css').'?v='.filemtime($fn)?>">
+		<link rel="stylesheet" href="<?=($fn='css/main.css').'?v='.filemtime($fn)?>">
+	</head>
+	<body>
+
+		<div class="content-wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">Test</div>
+					<div class="col-md-6">Test</div>
+				</div>
+			</div>
+		</div>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+		<script src="<?=($fn='js/bootstrap.min.js').'?v='.filemtime($fn)?>"></script>
+		<script src="<?=($fn='js/main.js').'?v='.filemtime($fn)?>"></script>
+
+		<script type="text/javascript">
+		</script>
+
+	</body>
+</html>
